7a69e6394e3d20a3a1f19a35376c43fa84882325
David Belanger <belanger@vangogh.cs.umass.edu>
2011-11-29 22:04:42 -0500

    adding updates to c3.m 2

--- code/getLocalPaths.m
+++
@@ -1,6 +0,0 @@
-function path = getLocalPaths()
-path.matlab = '/Users/belanger/bin/libsvm/matlab';
-path.glmnet = './glmnet_matlab';
-path.outdatadir = './data/';
-
-path.data = '../../homework/hw4/matlab/spam/spambase/spambase.data';
