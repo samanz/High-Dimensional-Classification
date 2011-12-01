938da28b0a6905d11001bf39c728a47c0625b6fa
Sam Anzaroot <sam@samanz.com>
2011-12-01 14:02:48 -0500

    Some changes to knnWrapper

--- code/knnWrapper.m
+++ code/knnWrapper.m
@@ -1,9 +1,6 @@
 function out  = knnWrapper(xTrain,yTrain,xTest,yTest,opt)
 
-    NS.X = xTrain;
-    tic
     IDX = knnsearch(xTrain,xTest,'K', opt.k, 'Distance', opt.distance);
-    toc
     a = zeros(length(IDX),1);
     for i=1:length(IDX)
         a(i) = mode(yTrain(IDX(i)));
