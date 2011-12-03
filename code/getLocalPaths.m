c3871a0dd09145064c46e429223af31724d0473d
David Belanger <belanger@vangogh.cs.umass.edu>
2011-12-02 20:13:38 -0500

    removing getLocalPaths.m

--- code/getLocalPaths.m
+++
@@ -1,8 +0,0 @@
-function path = getLocalPaths()
-path.matlab = '/Users/belanger/bin/libsvm/matlab';
-path.glmnet = './glmnet_matlab';
-path.outdatadir = './data/';
-
-%path.data = '../../homework/hw4/matlab/spam/spambase/spambase.data';
-path.data = '../data/smartrcv1.mat';
-path.PCA = '../data/PCA.mat';
