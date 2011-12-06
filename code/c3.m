90cc8784f7b13a5516c442c3a8285dc1f3f6a55f
David Belanger <belanger@vangogh.cs.umass.edu>
2011-12-06 09:37:23 -0500

    added crossval for when using full trainset

--- code/c3.m
+++ code/c3.m
@@ -28,8 +28,8 @@ yTest = All_lab((trainsize + devsize + 1):end,:);
 
 numfolds = 10;
 noreduce = @(x,y,z,w)deal(x,y,z);
-load_pca_data = 0;
-load_rand_data = 1;
+load_pca_data = 1;
+load_rand_data = 0;
 if(load_pca_data)
     PCA_Matrix = load(paths.PCA);  
 end
@@ -55,12 +55,18 @@ classifiers =        struct('svm',struct('function',@libsvmWrapper,'reduce',nore
                      'naivebayes_smooth_rand',  struct('function',@naiveBayesWrapper,'reduce',RP,'options', struct('smooth',1,'dim',50)));
 
 %Techniques = {'svm','ridge','naivebayes_nosmooth_pca','lasso'};%,'lasso_pca'};
-Techniques = {'naivebayes_smooth_rand'};
+Techniques = {'naivebayes_smooth_pca'};
 
 nT = length(Techniques);
 rate = zeros(1,nT);
-%train_set_sizes = [trainsize];
-train_set_sizes = floor(linspace(1000,trainsize,10));
+train_set_sizes = [trainsize];
+%train_set_sizes = floor(linspace(1000,trainsize,10));
+if(length(train_set_sizes) == 1 & train_set_sizes(1) == trainsize)
+    evaluator_style = 'crossval'
+else
+    evaluator_style = 'subsample'
+end
+
 if(runExperiments == 1)
 for train_set_size = train_set_sizes
 for Ti = 1:nT
@@ -75,8 +81,12 @@ for Ti = 1:nT
     for i = 1:numfolds
         tic 
         options.fold = i;
-        out(i) = subsample_and_reduce_and_classify(train_set_size,classifier,reduce,xTrain,yTrain,xDev,yDev,xTest,yTest,options);
-        times(i) = toc;
+        if(strcmp(evaluator_style,'subsample'))
+            out(i) = subsample_and_reduce_and_classify(train_set_size,classifier,reduce,xTrain,yTrain,xDev,yDev,xTest,yTest,options);
+        elseif(strcmp(evaluator_style,'crossval'))
+            out(i) = crossval_and_classify(train_set_size,classifier,reduce,xTrain,yTrain,xDev,yDev,xTest,yTest,options); 
+        end
+            times(i) = toc;
         disp(['finished iteration ' i]);
     end
 
