529264500821ea82d1769e07d845d3255bfed088
David Belanger <belanger@vangogh.cs.umass.edu>
2011-12-06 18:03:38 -0500

    fixed bug in libsvmwrapper options handling

--- code/libsvmWrapper.m
+++ code/libsvmWrapper.m
@@ -1,6 +1,6 @@
 function out = libsvmWrapper(xTrain,yTrain,xDev,yDev,xTest,yTest,opt)
 
-[a b c] = svmpredict(yTest,xTest,svmtrain(yTrain,xTrain,opt));
+[a b c] = svmpredict(yTest,xTest,svmtrain(yTrain,xTrain,opt.string));
 
 correct = (a == yTest);
 out = mean(correct);
