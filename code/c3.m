1ab781e948e5b0f8b78f71e3f067279fdb973c39
Sam Anzaroot <sam@samanz.com>
2011-12-07 20:15:07 -0500

    Added legend

--- code/c3.m
+++ code/c3.m
@@ -1,6 +1,6 @@
 clear All; close All;
 runExperiments = 1;
-%figure out how to automaticAlly select which lambda value to use in glmnet
+%figure out how to automatically select which lambda value to use in glmnet
 paths = getLocalPaths();
 addpath(paths.matlab);
 addpath(paths.glmnet);
@@ -30,10 +30,10 @@ yDev = All_lab((trainsize + 1):(trainsize + devsize),:);
 xTest = All((trainsize + devsize + 1):end,:);
 yTest = All_lab((trainsize + devsize + 1):end,:);
 
-numfolds = 2;
+numfolds = 10;
 noreduce = @(x,y,z,w)deal(x,y,z);
-load_pca_data = 0;
-load_rand_data = 0;
+load_pca_data = 1;
+load_rand_data = 1;
 if(load_pca_data && runExperiments)
     PCA_Matrix = load(paths.PCA);  
 end
@@ -48,6 +48,7 @@ RP = @(train,dev,test,options)rand_proj_read(train,dev,test,options,RAND_Matrix.
 nonnative_indices = (size(xTrain,2)/2):size(xTrain,2);
 
 classifiers =        struct('svm',struct('function',@libsvmWrapper,'reduce',noreduce,'options', struct('string','-t 0')), ...
+                     'svm_pca',struct('function',@libsvmWrapper,'reduce',PCA,'options', struct('string','-t 0','dim',50)), ...
                      'ridge',struct('function',@glmnetWrapper,'reduce',noreduce,'options', struct('family','binomial','alpha',0,'type','')),...
                      'lasso',struct('function',@glmnetWrapper,'reduce',noreduce,'options', struct('family','binomial','alpha',1,'type','')),...
                      'lassoSupport',struct('function',@glmnetWrapperFindSupport,'reduce',noreduce,'options', struct('family','binomial','alpha',1,'type','','nonnative_indices',nonnative_indices)),...
@@ -56,18 +57,20 @@ classifiers =        struct('svm',struct('function',@libsvmWrapper,'reduce',nore
                      'naivebayes_nosmooth_pca',  struct('function',@naiveBayesWrapper,'reduce',PCA,'options', struct('smooth',0,'dim',50)),...
                      'naivebayes_smooth',  struct('function',@naiveBayesWrapper,'reduce',noreduce,'options', struct('smooth',1,'dim',50)),...
                      'naivebayes_smooth_pca',  struct('function',@naiveBayesWrapper,'reduce',PCA,'options', struct('smooth',1,'dim',50)),...    
-                     'lasso_pca',struct('function',@glmnetWrapper,'reduce',PCA,'options', struct('family','binomial','alpha',1,'type','')),...
+                     'lasso_pca',struct('function',@glmnetWrapper,'reduce',PCA,'options', struct('family','binomial','alpha',1,'type','','dim',50)),...
+                     'naivebayes_smooth_pca_small',  struct('function',@naiveBayesWrapper,'reduce',PCA,'options', struct('smooth',1,'dim',5)),...    
                      'quad_analysis',struct('function',@matlabclassifierWrapper,'reduce',noreduce,'options', 'diagLinear'),...
                      'naivebayes_smooth_rand',  struct('function',@naiveBayesWrapper,'reduce',RP,'options', struct('smooth',1,'dim',50)));
 
-%Techniques = {'svm','ridge','naivebayes_smooth_pca','lasso','naivebayes_smooth_rand'};%,'lasso_pca'};
-Techniques = {'lasso'};%,'lasso_pca'};
+%Techniques = {'svm','svm_pca','ridge','lasso_pca','naivebayes_smooth_rand','naivebayes_smooth_pca_small','naivebayes_smooth_pca','lasso'};%,'lasso_pca'};
+%Techniques = {'lasso'};%,'lasso_pca'};
+Techniques = {'naivebayes_smooth'};%,'lasso_pca'};
 
 nT = length(Techniques);
 rate = zeros(1,nT);
-train_set_sizes = [trainsize];
+%train_set_sizes = [trainsize];
 %train_set_sizes = [100];
-%train_set_sizes = floor(linspace(1000,trainsize,10));
+train_set_sizes = floor(linspace(300,trainsize,10));
 if(length(train_set_sizes) == 1 && train_set_sizes(1) == trainsize)
     evaluator_style = 'crossval';
 else
@@ -114,25 +117,35 @@ for ii = 1:length(train_set_sizes);
         technique = Techniques{Ti};
         infile = [outdatadir technique '.' int2str(train_set_size) '.mat'];
         S = load(infile);
-        Accdata(Ti,ii,:) = cell2mat(S.out);
+        for i=1:numfolds
+            Accdata(Ti,ii,i) = S.out{i}.mean;
+        end;
         infile = [outdatadir technique '.' int2str(train_set_size) '.times.mat'];
         S = load(infile);
         Timedata(Ti,ii,:) = S.times;
     end
 end
-colors = {'red','blue','green','cyan','black','yellow','purple','cyan'};
+colors = {'red','blue','green','cyan','black','yellow','magenta',[.55 .44 .13]};
 hold on;
+legends = {}
+for i=1:length(Techniques)
+    a = Techniques{i};
+    a(find(a == '_')) = ' ';
+    legends{i} = a;
+end;
 for Ti = 1:nT
     %accuracy
     hold on; figure(1);
     E = std(Accdata(Ti,:,:),0,3);
     M = mean(Accdata(Ti,:,:),3);
     errorbar(train_set_sizes,M,E,'color',colors{Ti});
+    legend(legends);
     hold off;
     %time 
     hold on; figure(2);
     E = std(Timedata(Ti,:,:),0,3);
     M = mean(Timedata(Ti,:,:),3);
     errorbar(train_set_sizes,M,E,'color',colors{Ti});
+    legend(legends);
 end
 hold off;
